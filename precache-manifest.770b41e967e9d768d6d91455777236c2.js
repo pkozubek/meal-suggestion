5835b3cd759a6113f3ed4f7bd68f83c586678939
kozi <pkozubek92@gmail.com>
2019-07-20 21:01:27 +0200

    Updates

---
+++ precache-manifest.770b41e967e9d768d6d91455777236c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d28a746df20e6af0eea7a11f510e7cf",
+    "url": "/meal-suggestion/index.html"
+  },
+  {
+    "revision": "36e6c89eae71b101c388",
+    "url": "/meal-suggestion/static/css/main.112eb524.chunk.css"
+  },
+  {
+    "revision": "19179a4ff06c2680614d",
+    "url": "/meal-suggestion/static/js/2.ec6ec3ae.chunk.js"
+  },
+  {
+    "revision": "36e6c89eae71b101c388",
+    "url": "/meal-suggestion/static/js/main.aacd2926.chunk.js"
+  },
+  {
+    "revision": "5fb24a81bedc168bbc8b",
+    "url": "/meal-suggestion/static/js/runtime~main.1bb10395.js"
+  }
+]);
